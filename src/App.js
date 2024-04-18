b6d087289a913ae3ba39b400079a364935f6cdb9
Nico0! <nicouzu96@hotmail.es>
2024-04-18 17:19:54 -0300

    spa-initial-commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import 'bootstrap/dist/css/bootstrap.css';
+import './App.css'; 
+import Home from './component/Home';
+
+function App() {
+  return (
+    <div class="container">
+      <header className="App-header"> 
+     
+      </header>
+      <Home/>
+    </div>
+  );
+}
+
+export default App;
