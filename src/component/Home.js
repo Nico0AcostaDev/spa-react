e4a7cf644bf1c5048be2a5147a4201f028af6c27
Nico0! <nicouzu96@hotmail.es>
2024-04-19 12:30:02 -0300

    Home styles

--- src/component/Home.js
+++ src/component/Home.js
@@ -2,11 +2,37 @@ import React, { Component } from 'react'
 
 export default  class Home extends Component {
 
-  render() {
-
-
+  render() { 
     return (
-      <div class="container">prueba</div>
+      <div class="container-sm bg-primary py-3 bg-opacity-25 text-center shadow-sm overflow-auto">  
+      <div class="row">
+        <div class="col-md-6">
+          <h3>Proyectos Disponibles</h3>
+          <div class="card bg-light my-3"> 
+            <div class="card-body">  
+              <h5 class="card-title">Card title</h5>
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div> 
+          </div>
+        </div>
+        <div class="col-md-6">
+          <h3>Servicios disponibles</h3>
+          <div class="card bg-light my-3"> 
+            <div class="card-body">  
+              <h5 class="card-title">Card title</h5>
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div> 
+          </div>
+        </div>
+      </div>
+    </div>
+    
+    
+
+
+
     )
   }
 }
