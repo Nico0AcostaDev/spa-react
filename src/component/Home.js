9980c978a34a7c9343ad8392ab26c6e0aa9ba878
Nico0! <nicouzu96@hotmail.es>
2024-04-19 14:17:09 -0300

    fix-methods

--- src/component/Home.js
+++ src/component/Home.js
@@ -1,8 +1,34 @@
-import React, { Component } from 'react'
+import React, { useState, useEffect } from 'react'
 
-export default  class Home extends Component {
+function Home() {
 
-  render() { 
+  const [servicio, setServicio] = useState(null); 
+
+  const obtenerServicios = () => {
+    
+    var myHeaders = new Headers();
+    myHeaders.append("x-rapidapi-key", "XxXxXxXxXxXxXxXxXxXxXxXx");
+    myHeaders.append("x-rapidapi-host", "v3.football.api-sports.io");
+     
+    fetch("https://api.football-data.org/v4/matches", {
+        method: 'GET',
+        headers: myHeaders,
+        redirect: 'follow'
+    })
+    .then(response => response.json()) 
+  .then(data => {
+    // Aquí puedes trabajar con los datos recibidos en la respuesta
+    console.log(data);
+    // Por ejemplo, puedes devolver los datos aquí si los necesitas
+   setServicio(data);
+  })
+  }
+    
+  
+  useEffect(() => {
+    obtenerServicios();
+}, []);
+
     return (
       <div class="container-sm bg-primary py-3 bg-opacity-25 text-center shadow-sm overflow-auto">  
       <div class="row">
@@ -11,7 +37,7 @@ export default  class Home extends Component {
           <div class="card bg-light my-3"> 
             <div class="card-body">  
               <h5 class="card-title">Card title</h5>
-              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              <p class="card-text">asd</p>
               <a href="#" class="btn btn-primary">Go somewhere</a>
             </div> 
           </div>
@@ -35,4 +61,6 @@ export default  class Home extends Component {
 
     )
   }
-}
+ 
+export default Home;
+
